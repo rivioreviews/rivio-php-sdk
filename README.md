47581c7bfe1dc842a585d71c3a26e2c1fc3ab817
Pesti Gyorgy <pesti@nomo.hu>
2015-12-10 13:52:48 +0100

    fix

--- README.md
+++ README.md
@@ -1,3 +1,53 @@
 Rivio PHP SDK
 =============
 
+The Rivio PHP SDK provides integration access to the Rivio API.
+
+## Dependencies
+
+PHP version >= 5.2.0 is required.
+
+The following PHP extensions are required:
+
+* curl
+
+## Install and configure
+
+###Get started with our PHP SDK by hitting the download link below.
+
+###Or use composer
+
+[Composer](http://getcomposer.org/doc/01-basic-usage.md) is a package manager for PHP. In the `composer.json` file in your project add:
+
+```javascript
+{
+  "require" : {
+    "rivio/rivio-php-sdk": "*"
+  }
+}
+```
+
+And then run:
+
+    php composer.phar install
+
+## Quick Start Example
+
+```php
+<?php
+
+require_once 'PATH_TO_RIVIO_PHP_SDK/src/Rivio.php';
+
+//Copy credentials from Rivio Dashboard (http://dashboard.reev.io/dashboard/settings/business)
+$rivio = new Rivio('api_key','secret_key','ID');
+
+
+$result=$rivio->register_postpurchase_email();
+
+var_dump($result);
+```
+
+## License
+
+See the LICENSE file.
+
--- src/Rivio.php
+++ src/Rivio.php
@@ -2,6 +2,70 @@
 
 
 class Rivio {
+    private static $api_base_url='https://api.reev.io/api';
+    private $api_key=NULL;
+    private $secret_key=NULL;
+    private $business_id=NULL;
 
+    function __construct($api_key = NULL,$secret_key = NULL,$business_id = NULL){
+        $this->api_key=$api_key;
+        $this->secret_key=$secret_key;
+        $this->business_id=$business_id;
+    }
 
+    public function get_embed_html(){
+
+    }
+
+    public function register_postpurchase_email(){
+        $order=array(
+            "order_id"=> "1492411012",
+            "ordered_date"=>"2015-09-28T09:16:16-04:00",
+            "customer_email"=>"example@email.com",
+            "customer_first_name"=>"Joe",
+            "product_id"=>"1621557380",
+            "product_name"=>"Example product",
+            "product_barcode"=>"545241324753",
+            "product_category"=>"Smart phone",
+            "product_url"=>"http://reevio-dani-test.myshopify.com/products/nexus-5-lg-d821",
+            "product_image_url"=>"https://cdn.shopify.com/s/files/1/0981/5786/products/415925.lg-e960-nexus-4-16gb_1024x1024.jpg?v=1441359580",
+            "product_description"=>"This is the product description",
+            "product_brand"=>"Example brand",
+            "product_price"=>"18.00"
+        );
+
+        $orders=array($order);
+
+        return $this->register_postpurchase_emails($orders);
+    }
+
+    public function register_postpurchase_emails($orders){
+        $postBody=array(
+            "orders"=> $orders
+        );
+
+        // Setup cURL
+        $ch = curl_init(static::$api_base_url.'/postpurchase?business_id='.$this->business_id.'&reevio_secret_key='.$this->secret_key.'');
+        curl_setopt_array($ch, array(
+            CURLOPT_POST => TRUE,
+            CURLOPT_RETURNTRANSFER => TRUE,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json'
+            ),
+            CURLOPT_POSTFIELDS => json_encode($postBody)
+        ));
+
+        // Send the request
+        $response = curl_exec($ch);
+
+        // Check for errors
+        if($response === FALSE){
+            throw new Exception(curl_error($ch));
+        }
+
+        // Decode the response
+        $responseData = json_decode($response, TRUE);
+
+        return $responseData;
+    }
 }
