7535b9f791440c8073f67d5a46d31105281dcee1
Daniel Rados <danielrados1990@gmail.com>
2017-05-03 15:39:49 +0200

    product reviews json example formatting

--- examples/product_reviews_json.php
+++ examples/product_reviews_json.php
@@ -3,6 +3,16 @@
 require_once(__DIR__ . "/bootstrap.php");
 
 $rivio_reviews_json = $rivio->product_reviews_json('1492411012');
-$rivio_reviews_json = json_encode($rivio_reviews_json);
-
-echo $rivio_reviews_json;
+$rivio_reviews_json = json_encode($rivio_reviews_json, JSON_PRETTY_PRINT);
+?>
+<html>
+<head>
+    <title>Product reviews JSON</title>
+</head>
+<body>
+<h1>Product reviews JSON</h1>
+<pre style="background: #eeeeee; padding: 8px;">
+<?php echo $rivio_reviews_json;?>
+</pre>
+</body>
+</html>
